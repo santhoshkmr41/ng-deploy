823c82e50e39e09acdc8277a64a11a83816b6001
Santhosh kumar <santhoshkmr41@gmail.com>
2024-04-08 22:52:48 +0530

    project init

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'project-1-aj17';
+  isAdmin=true;
+  ingredientList = [
+    {name: 'noodles', quantity: 1},
+    {name: 'miso broth', quantity: 1},
+    {name: 'egg', quantity: 2},
+  ];
+  
+}
